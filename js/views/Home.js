1fb63b0203ffb7087d830417187291eea90b9acc
Justin Quinn <Justin.Quinn09@gmail.com>
2022-07-27 14:25:21 -0500

    added the ability to show picture tiles, formated a second view screen to show TV SHOWS. took footer off of apsolute location.

--- js/views/Home.js
+++ js/views/Home.js
@@ -3,26 +3,57 @@
 const BASE_URI = `${BACKEND_HOST}/api/s3/download`;
 export default function Home(props) {
     let html =  `
-        <header><h1>Home Page</h1></header>
-        <main><div class="gallery">
+        <main>
+        <h1>Movies</h1>
+        <div class="gallery">
     `;
-    for (let i = 0; i < props.movies.length; i++) {
+    console.log(props);
+    for (let i = 0; i < 75; i++) {
         html += `
-        <div class="movie">
-            <div class="poster">
-             
+        <div class="movie flip-card" xmlns="http://www.w3.org/1999/html">
+            <div class="poster flip-card-inner">
+            <div class="flip-card-front">
+                <img src="../../Database Files/covers/${props.movies[i].id}.jpg" alt="${props.movies[i].name}">
             </div>
-            <div class="name">
-                ${props.movies[i].name}
+            <div class="flip-card-back">
+                <div class="name">
+                    <p>${props.movies[i].name}</p>
+                    <p>genre: ${props.movies[i].genre}</p>
+                    <p>year: ${props.movies[i].year}</p>
+                    <p>director: ${props.movies[i].director}</p>
+                    <p>rating: ${props.movies[i].rating}</p>
+                </div>
+                <div class="movie-footer"><a href="/edit" data-link>Edit</a></div>
             </div>
-            <div class="movie-footer"><a href="/edit" data-link>Edit</a></div>
+        </div>
+        </div>
+        `;
+    }
+    html += `</div><br><h1>Tv Shows</h1><div class="gallery">`;
+        for (let i = 0; i < 75; i++) {
+            html += `
+        <div class="movie flip-card">
+            <div class="poster flip-card-inner">
+            <div class="flip-card-front">
+                <img src="../../Database Files/covers/${props.tvShows[i].id}.jpg" alt="${props.tvShows[i].name}">
+            </div>
+            <div class="flip-card-back">
+                <div class="name">
+                    <p><b>${props.tvShows[i].name}</b></p>
+                    <p>Genre: ${props.tvShows[i].genre}</p>
+                    <p>Year: ${props.tvShows[i].year}</p>
+                    <p>Director: ${props.tvShows[i].director}</p>
+                    <p>Rating: ${props.tvShows[i].rating}</p>
+                </div>
+                <div class="movie-footer"><a href="/edit" data-link>Edit</a></div>
+            </div>
+        </div>
         </div>
         `;
     }
     html += `</div></main>`;
     return html;
 }
-//   <img src="${props.movies[i].id}.jpg" alt="${props.movies[i].id}">
 export function HomeEvents() {
 
 
